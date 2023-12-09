4417248daeec992cc1ea7647911ebfa583fd9d2f
Shadrach Arowolo <princetunde17@gmail.com>
2023-12-09 22:21:00 +0100

    form validation

--- app.js
+++ app.js
@@ -11,24 +11,26 @@ const nameError = document.getElementById('name-error');
 const emailError = document.getElementById('email-error');
 const messageError = document.getElementById('message-error');
 const formBtn = document.getElementById('submit-btn');
+const submitError = document.getElementById('submitError');
 
 function validateName() {
-    let contactName = document.getElementById('name').value;
-    let nameRegex = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    const contactName = document.getElementById('name').value;
+    // const nameRegex = /^[a-zA-Z]+ [a-zA-Z]+$/;
     if (contactName.length == 0) {
         nameError.innerHTML = "Please type a name";
         return false;
-    } else if (!contactName.match(nameRegex)) {
-        nameError.innerHTML = "Full name required";
-        return false;
+    // } else if (!contactName.match(nameRegex)) {
+    //     nameError.innerHTML = "Full name required";
+    //     return false;
     } else {
-        nameError.innerHTML = "valid";
+        nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+        return true
     }
 }
 
 function validateEmail() {
-    let emailId = document.getElementById('email');
-    let emailRegex = /^[a-zA-Z0-9. _-]+@[a-zA-Z0-9. -]+\. [a-zA-Z]{2,4}$/;
+    const emailId = document.getElementById('email').value;
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (emailId.length == 0) {
         emailError.innerHTML = "Email required";
         return false;
@@ -36,6 +38,28 @@ function validateEmail() {
         emailError.innerHTML = "Please enter a valid email address";
         return false;
     } else {
-        emailError.innerHTML = "valid";
+        emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+        return true;
+    }
+}
+
+function validateMessage() {
+    const messageBox = document.getElementById('message').value;
+    if (messageBox.length == 0) {
+        messageError.innerHTML = "Please type a Message";
+        return false;
+    } else {
+        messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+        return true;
+    }
+}
+
+function validateForm() {
+    if (!validateName() || !validateEmail() || !validateMessage) {
+        submitError.innerHTML = "Please Enter Correct Details";
+        return false;
+    } else {
+        submitError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+        return true;
     }
 }
